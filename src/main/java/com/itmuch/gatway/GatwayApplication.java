cc324f443c37e97ed387e95713ad47946f8bdd23
wang guo <2431605485@qq.com>
2026-04-10 16:21:09 +0800

    Initial commit

---
+++ src/main/java/com/itmuch/gatway/GatwayApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatwayApplication.class, args);
+    }
+
+}
